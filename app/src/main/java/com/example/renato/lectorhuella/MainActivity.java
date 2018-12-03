75c2eff4de5509335d86db20697db3aea6d12fcf
renatoillescar <rillesca@espol.edu.ec>
2018-12-02 22:22:38 -0500

    prototipo de lector de huella dactilar

---
+++ app/src/main/java/com/example/renato/lectorhuella/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.renato.lectorhuella;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
+
+
